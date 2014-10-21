5ec53df7329e88391e10a9a33c69c03f696f5bc6
xiao <zghanxiao@gmail.com>
2014-10-21 16:29:45 -0700

    changeGenericClassView

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
-#jadmin.autodiscover()
+from snippets import views
+
 
 urlpatterns = patterns(
-	'snippets.views',
+	'',
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^snippets/$', 'snippet_list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+    url(r'^snippets/$', views.SnippetList.as_view()), 
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 #    url(r'^', include('snippets.urls')),
 )
 urlpatterns = format_suffix_patterns(urlpatterns)
