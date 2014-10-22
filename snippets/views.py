ecace73af0426381eb3c06bc6997342a7ed7231e
xiao <zghanxiao@gmail.com>
2014-10-22 11:34:37 -0700

    authorization

--- snippets/views.py
+++ snippets/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 class SnippetList(generics.ListCreateAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+	def pre_save(self, obj):
+		obj.owner = self.request.user
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+	def pre_save(self, obj):
+		obj.owner = self.request.user
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,5 +13,8 @@ urlpatterns = patterns(
     url(r'^snippets/$', views.SnippetList.as_view()), 
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 #    url(r'^', include('snippets.urls')),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
 )
 urlpatterns = format_suffix_patterns(urlpatterns)
