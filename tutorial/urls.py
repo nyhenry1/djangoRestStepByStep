8a401b88b18b3fdfdc4d58b7fe439aa39ac5e309
xiao <zghanxiao@gmail.com>
2014-10-22 12:21:56 -0700

    add api hyperlinks

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -5,16 +5,23 @@ from snippets import views
 
 
 urlpatterns = patterns(
-	'',
+	'snippets.views',
+	
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^snippets/$', views.SnippetList.as_view()), 
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^$', 'api_root'),
+    url(r'^snippets/$', views.SnippetList.as_view(),
+			name = 'snippet-list'), 
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(),
+			name = 'snippet-detail'),
 #    url(r'^', include('snippets.urls')),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(),
+						name = 'snippet-highlight'),
+    url(r'^users/$', views.UserList.as_view(),
+			name = 'user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(),
+			name = 'user-detail'),
     url(r'^api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
 )
 urlpatterns = format_suffix_patterns(urlpatterns)
